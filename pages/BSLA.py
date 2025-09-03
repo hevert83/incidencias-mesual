58681b12a0436519c7484e4ee9c49528d93801a3
hevert83 <101134285+hevert83@users.noreply.github.com>
2025-09-03 04:02:36 -0600

    Update BSLA Agosto.py
    Datos al conteo correspondiente al mes de Agosto

--- pages/BSLA.py
+++ pages/BSLA.py
@@ -7,33 +7,35 @@ import plotly.express as px
 
 # set page configuration
 st.set_page_config(
-    page_title="Dashboard de Incidencias BSMR",
+    page_title="Dashboard de Incidencias BSLA",
     page_icon=":bar_chart:",
     layout="wide",  
     
 )
-col1, col2, col3 = st.columns([1, 6, 1])
+
+col1, col2, col3 = st.columns([1, 2, 1])
 
 with col1:
-    st.image("img/logo_bsmr.png", width=100)
-
+    st.image("img/logo_bsmr.png", width=200)
+    
+with col2:   
+    st.title("Dashboard de Incidencias BSLA")
 with col3:
-    st.image("img/Logo1.png", width=100)
+    st.image("img/logo1.png", width=170)
 
 
 #Titulo principal
-st.title("Dashboard de Incidencias BSMR")
-st.markdown("Análisis de las incidencias reportadas en la planta BSMR durante el mes de febrero de 2025.")
+st.markdown("Análisis de las incidencias reportadas en la planta BSMR durante el mes de Agosto del 2025.")
 
 with st.expander("Descripción del Dashboard" , expanded=True):
     st.markdown("""
-    Este dashboard presenta un análisis detallado de las incidencias reportadas en la planta BSMR durante el mes de febrero de 2024. 
+    Este dashboard presenta un análisis detallado de las incidencias reportadas en la planta BSMR durante el mes Agosto del 2025. 
     Incluye gráficos y tablas que muestran la distribución de las incidencias por categoría, nivel de gravedad y área afectada.
     """)
 
 # Cargar los datos desde el archivo CSV
 try:
-    incidencias_df = pd.read_csv('data/03_formateado.csv')
+    incidencias_df = pd.read_csv('data/Reporte de incidencias(BSLA) Agosto_formateado.csv')
     #st.success("Datos cargados correctamente desde 'incidencias_febrero.csv'.")
     
     
